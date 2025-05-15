f9cc4573ee4791c28e501f4e0f9954a9abfd45b7
Vas_G21 <vasilenko.georgy2018@gmail.com>
2025-05-15 22:03:36 +0300

    Update app.py

--- itmo_project/dashboard/app.py
+++ itmo_project/dashboard/app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+from scripts.visualization import plot_girls_distribution
 
+st.title("Анализ русских рэп-клипов")
+df = pd.read_csv('../data/rap_videos.csv')
+
+st.header("Основные статистики")
+st.dataframe(df.describe())
+
+st.header("Распределение девушек по клипам")
+plot_girls_distribution(df)
+st.pyplot()
